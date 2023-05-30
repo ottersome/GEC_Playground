e2fad03e671631cfc906c23fbdbcf8f8f5b55f8b
Luis Garcia <luisgarcia26284@gmail.com>
2023-05-30 16:23:52 +0800

    Initial Commit. Data formed.

---
+++ wi+locness/data_processing.py
@@ -0,0 +1,101 @@
+import sys
+import os
+import argparse
+import pandas as pd
+import pathlib 
+import matplotlib.pyplot as plt
+
+hist = {}
+
+def argsyparsy():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '--data_directory',
+            dest='data_directory',
+            required=True
+            )
+    argparser.add_argument(
+        '--dest_dir',
+            dest='dest_dir',
+            required=True
+            )
+    return argparser.parse_args()
+
+def get_files_in_dir(dire:pathlib.Path):
+
+    if not isinstance(dire, pathlib.Path):
+        raise Exception('Not a directory')
+    listo = []
+    for f in dire.iterdir():
+        if 'train' in f.name and f.name[0] != '.':
+            listo.append(f)
+    return listo
+
+def add_count(err_type):
+    # err_type = err_type.replace(':','')
+    if not err_type in hist.keys():
+        hist[err_type] = 0
+    else:
+        hist[err_type] += 1
+
+def collect_examples_in_file(file_path:pathlib.Path):
+    # Given Initial Positio
+    # Start Loop
+    errs = []
+    print('Reading file: ', file_path)
+    with file_path.open() as f:
+        # Get S
+        while True:
+            line = f.readline()
+            if line == '': break # EOF 
+            assert line[0] == 'S'
+            src_sentence = line[2:]
+            
+            line = f.readline()
+            err_types = []
+            while line != '\n':
+                assert line[0] == 'A', print('Line was {}'.format(line))
+                # Break Line Into Consituent Parts
+                err_type = line.split('|||')[1]
+                err_types.append(err_type)
+                add_count(err_type)
+                #print('Error type : ',err_type)
+                line = f.readline()
+            err_types = '\t'.join(err_types)
+            errs.append((src_sentence,err_types))
+    return errs
+
+def main(src_dir,save_path):
+    # For Each File in Directory
+    errs = []
+    for file in get_files_in_dir(src_dir):
+        file_errs = collect_examples_in_file(file)
+        errs = errs+file_errs
+    
+    # print(errs)
+    # Create New Pandas DataFrame
+    df = pd.DataFrame(data = errs, columns=('source', 'errs'))
+    
+    # Dump it all in a single File
+    df.to_csv(save_path)
+
+if __name__=='__main__':
+    args = argsyparsy()
+    if not pathlib.Path(args.data_directory).exists() or not pathlib.Path(args.data_directory).is_dir():
+        print('Invalid Input Path. Not an Existing Directory')
+        exit(-1)
+    # if not pathlib.Path(args.dest_dir).():
+        # print('Invalid Output Path. Not an Existing File')
+        # exit(-1)
+    # Else Create a Directory Object
+    src = pathlib.Path(args.data_directory)
+    dest = pathlib.Path(args.dest_dir)
+    main(src,dest)
+
+    # Show Distribution 
+    plt.barh(range(len(hist)),hist.values())
+    plt.yticks(range(len(hist)),labels=hist.keys())
+    plt.title('Distribution of Errors')
+    plt.legend()
+    plt.show()
+    print('Done.')
